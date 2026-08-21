f6e9b9e03286b9e4e5e0467f4dcb448f32d159e9
EvgenSharaban <evgen.sharaban@itechcraft.com>
2026-08-21 14:00:26 +0300

    CT-31: changed serverUrl in WebSocketConfig for working with Render (not local)

--- lib/core/config/websocket_config.dart
+++ lib/core/config/websocket_config.dart
@@ -4,5 +4,8 @@ class WebSocketConfig {
 
   // Используем 10.0.2.2 (вместо localhost) если используем Android emulator
   // Если порт бэкенда отличается от 3000, измените его здесь.
-  static const String serverUrl = 'ws://localhost:8080';
+  // static const String serverUrl = 'ws://localhost:8080';
+
+  // Заменяем localhost на URL от Render
+  static const String serverUrl = 'wss://collabtasks-backend.onrender.com';
 }
